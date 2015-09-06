bbcf484f55f1d2286d815b1bc6167163b40bcbb4
Nikola Temelkovski <temelkovski_n@yahoo.com>
2015-09-06 17:12:52 +0200

    Added admin panel & changing video player

--- Watch.Me/Controllers/HomeController.cs
+++ Watch.Me/Controllers/HomeController.cs
@@ -5,17 +5,38 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using Microsoft.AspNet.Identity;
+using Watch.Me.Models;
+using Watch.Me.Models.ViewModels;
 
 namespace Watch.Me.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+
+
         public ActionResult Index()
         {
             string userId = User.Identity.GetUserId();
             //toDO fix
-            
+
+            bool isAdmin = User.IsInRole("admin");
+            //if (isAdmin)
+            //{
+            //    return View("~/Views/Admin/AdminHome.cshtml");
+            //}
+
+            var allVideos = _dbContext.Videos
+                .Where(v => v.IsApproved.Equals(true))
+                .Select(x => new UploadedVideos()
+            {
+                Id = x.Id,
+                Url = x.Url,
+                VideoTitle = x.VideoTitle
+            });
+
             return View();
+
         }
 
         public ActionResult About()
