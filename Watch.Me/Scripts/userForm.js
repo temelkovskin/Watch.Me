506737a5632a778a36570661015f18de1bb9131d
Kikser <kr.arsovski@gmail.com>
2015-09-09 18:12:17 +0200

    New PartialView, user Form is 70% done and something in css about this form

---
+++ Watch.Me/Scripts/userForm.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+    App.init();
+});
+var App = {
+    init: function () {
+        this.side.nav()
+    },
+    title: function (e) {
+        return $(".header>.title").text(e)
+    },
+    side: {
+        nav: function () {
+            this.toggle(), this.navigation()
+        },
+        toggle: function () {
+            $(".title").on("touchstart click", function (e) {
+                e.preventDefault(), $(".sidebar").toggleClass("active"), $(".nav").removeClass("active"), $(".sidebar .sidebar-overlay").removeClass("fadeOut animated").addClass("fadeIn animated")
+            }), $(".sidebar .sidebar-overlay").on("touchstart click", function (e) {
+                e.preventDefault(), $(".ion-ios-navicon").click(), $(this).removeClass("fadeIn").addClass("fadeOut")
+            })
+        },
+        navigation: function () {
+            $(".nav-left a").on("touchstart click", function (e) {
+                e.preventDefault();
+                var t = $(this).attr("href").replace("#", "");
+                $(".sidebar").toggleClass("active"), $(".html").removeClass("visible"), "home" == t || "" == t || null == t ? $(".html.welcome").addClass("visible") : $(".html." + t).addClass("visible"), App.title($(this).text())
+            })
+        }
+    }
+};
