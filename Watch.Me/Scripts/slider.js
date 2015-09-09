80ec1debcda6a898a1f1c4d651c2e6e31685d86c
Kikser <kr.arsovski@gmail.com>
2015-09-09 02:38:45 +0200

    Index is almost done

---
+++ Watch.Me/Scripts/slider.js
@@ -0,0 +1,9 @@
+﻿var width = $('.previews').width() / 3;
+$('.previews').bxSlider({
+    pager: false,
+    slideWidth: width,
+    minSlides: 3,
+    maxSlides: 3,
+    slideMargin: 10,
+    easing: 'ease-in-out-circ'
+});
